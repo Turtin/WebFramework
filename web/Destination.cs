0969b7463e33dffef3368874c1d20b95b16d27aa
Turtin <troypeiris@gmail.com>
2026-08-11 22:15:25 +1000

    Made some progress on getting the path handling, not complete and untested

--- web/Destination.cs
+++ web/Destination.cs
@@ -5,6 +5,6 @@ namespace Web_Framework.web;
 [AttributeUsage(AttributeTargets.Method)]
 public class Destination(string path, RequestMethods method) : Attribute
 {
-    public string path { get; } = path;
-    public RequestMethods method { get; } = method;
+    public string Path { get; } = path;
+    public RequestMethods Method { get; } = method;
 }
--- web/WebPaths.cs
+++ web/WebPaths.cs
@@ -12,5 +12,55 @@ public class WebPaths
     public void AddPath(Func<StatusCode> service)
     {
         Destination serviceInfo = service.Method.GetCustomAttribute<Destination>() ?? throw new InvalidServiceHandlerException(service);
+        string path = serviceInfo.Path;
+        string[] segments = path.Split('/');
+        
+        // Handle case where the path is at the head (most cases probably)
+        if (segments.Length == 1)
+        {
+            _paths.AddData(service);
+            return;
+        }
+
+        Tree<Func<StatusCode>>.Node<Func<StatusCode>> currentNode = _paths.GetNode(segments[0]);
+        
+        foreach (string segment in path.Split('/'))
+        {
+            segments = segments.Skip(1).ToArray();
+            
+            if (segments.Length == 1)
+            {
+                currentNode.AddData(service);
+                return;
+            }
+
+            if (currentNode.Children.ContainsKey(segment))
+            {
+                currentNode = currentNode.GetNode(segment);
+            }
+            else
+            {
+                currentNode.AddNode(segment, service);
+                currentNode = currentNode.GetNode(segment);
+            }
+        }
+    }
+
+    public void AddContent(Type contentClass)
+    {
+        List<MethodInfo> methods = contentClass.GetMethods().ToList();
+
+        foreach (MethodInfo method in methods)
+        {
+            if (method.GetCustomAttribute<Destination>() != null)
+            {
+                AddPath((Func<StatusCode>) Delegate.CreateDelegate(typeof(Func<StatusCode>), method));
+            }
+        }
+    }
+    
+    public Func<StatusCode> GetPath(string path)
+    {
+        return null; // temp
     }
 }
